2a6bd472dd80c4eaadc26ccc67ad3d9b9b76afe6
Sarthak Sarthi <sarthiisarthak@gmail.com>
2024-01-26 06:39:02 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const initdata = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+async function main() {
+  await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initdata.data = initdata.data.map((obj) => ({
+    ...obj,
+    owner: "65b0491545955ed5894ce3e6",
+  }));
+  await Listing.insertMany(initdata.data);
+  console.log("Data Was initialized");
+};
+
+initDB();
